1636d80de81db7a6f65fb8aa48889ac301b126b1
Mullvadsmacka <joel@monstermatte.se>
2022-09-21 13:25:02 +0100

    Credits

--- Assets/HitboxBoss.cs
+++ Assets/HitboxBoss.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class HitboxBoss : MonoBehaviour
 {
     [SerializeField] private GameObject cameraComponent;
-
+    [SerializeField] private float timer;
+    private bool activeTimer = false;
     private void OnTriggerEnter2D(Collider2D collision) 
     {
         if(collision.gameObject.CompareTag("Player") == true)
@@ -15,7 +17,20 @@ public class HitboxBoss : MonoBehaviour
             collision.gameObject.GetComponent<SpriteRenderer>().enabled = false;
             //collision.gameObject.GetComponent<PlayerState>().Respawn();
             //Måste restarta level också
+
           
+        }
+
+        void Update()
+        {
+            if(activeTimer == true)
+            {
+                timer -= Time.deltaTime;
+                if(timer <= 0)
+                {
+                   // SceneManager.SetActiveScene(1);
+                }
+            }
         }
     }
 }
