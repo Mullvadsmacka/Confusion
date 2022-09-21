f356aa3f829147ea642240ecf2edc1a903bcfd65
theepicone1997 <oscar-sture@live.se>
2022-09-21 23:15:42 +0200

    LatestChanges

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,17 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class Enemy : MonoBehaviour
 {
     private Animator animator;
     public float speed = 5;
     private float direction = 1f;
-
     public GameObject groundCheck;
     private bool isGrounded;
     Rigidbody2D rb;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -21,28 +18,19 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-
         Vector3 newPosition = gameObject.transform.position;
         newPosition.x += direction * speed * Time.fixedDeltaTime;
         rb.MovePosition(newPosition);
         GroundCeck();
-
         if (isGrounded == false)
         {
             ChangeDirection();
         }
-
     }
-
-
-
     private void GroundCeck()
     {
         isGrounded = false;
-
-
         Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheck.transform.position, 0.2f);
-
         for (int i = 0; i < colliders.Length; i++)
         {
             if (colliders[i].gameObject != gameObject)
@@ -50,27 +38,20 @@ public class Enemy : MonoBehaviour
                 isGrounded = true;
 
             }
-
         }
-
     }
-
-
-    private void ChangeDirection()
+    public void ChangeDirection()
     {
         direction = -direction;
         Vector3 currenScale = gameObject.transform.localScale;
         currenScale.x = direction;
         gameObject.transform.localScale = currenScale;
-
     }
-
-
     private void OnCollisionEnter2D(Collision2D collision)
-       {
-            if (collision.gameObject.CompareTag("Player") == true)
-            {
+    {
+        if (collision.gameObject.CompareTag("Player") == true)
+        {
             ChangeDirection();
         }
-}
+    }
 }
